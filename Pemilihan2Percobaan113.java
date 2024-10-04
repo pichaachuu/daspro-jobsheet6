ee5b3468387e7726946546ddb710a2f2b0163ba7
pichaachuu <nafisahaliyah01@gmail.com>
2024-10-04 19:46:11 +0700

    tahun kabisat

--- Pemilihan2Percobaan113.java
+++ Pemilihan2Percobaan113.java
@@ -8,10 +8,14 @@ public class Pemilihan2Percobaan113 {
         int tahun = input13.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
-                System.out.println("Tahun Kabisat");    
+            if ((tahun % 100) == 0) {
+               if ((tahun % 400) == 0) {
+                    System.out.println("Tahun kabisat");
+               } else
+               System.out.println("Bukan tahun kabisat");   
             } else
-                System.out.println("Bukan tahun kabisat");
-        }
+                System.out.println("Tahun kabisat");
+        } else
+        System.out.println("Bukan tahun kabisat");
     }
 }
